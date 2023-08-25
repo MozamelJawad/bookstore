78caf3b30fb90878a30ea3fa75fc993b9ab9d93b
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-25 11:19:50 +0430

    add books component

---
+++ src/components/Category.js
@@ -0,0 +1,10 @@
+const Category = () => {
+  const headingStyle = {
+    display: 'flex',
+    color: '#FFAA11',
+    marginTop: '2rem',
+    justifyContent: 'center',
+  };
+  return <h3 style={headingStyle}>This page will be designed later!</h3>;
+};
+export default Category;
