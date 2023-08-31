ab88c7ab463c842dabe949e313fec4c009647749
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-31 23:08:28 +0430

    add more style for bookstore CMS app

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <main className="main">
       <Navbar />
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="/categories" element={<Category />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
